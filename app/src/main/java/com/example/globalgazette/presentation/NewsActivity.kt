3a397bccc1ede0ebee4a6790f2bc77b74d6cf92b
Amlan101 <amlan.sahooricky@gmail.com>
2024-06-28 20:53:12 +0530

    Initial Commit with the complete UI design

---
+++ app/src/main/java/com/example/globalgazette/presentation/NewsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.globalgazette.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.globalgazette.R
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+
+    }
+}
