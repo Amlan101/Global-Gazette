1f3dbda867627ccc62ad74a9c117e04f22f84a62
Amlan101 <amlan.sahooricky@gmail.com>
2024-06-30 19:10:05 +0530

    Added the ViewModel and the ViewModelProviderFactory

--- app/src/main/java/com/example/globalgazette/util/Resource.kt
+++ app/src/main/java/com/example/globalgazette/util/Resource.kt
@@ -2,10 +2,10 @@ package com.example.globalgazette.util
 
 sealed class Resource<T>(
     val data: T? = null,
-    val message: T? = null
+    val message: String? = null
 )
 {
     class Success<T>(data: T?): Resource<T>(data)
-    class Error<T>(data: T? = null, message: T?): Resource<T>(data, message)
+    class Error<T>( message: String, data: T? = null): Resource<T>(data, message)
     class Loading<T>(): Resource<T>()
 }
